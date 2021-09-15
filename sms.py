aff5bb75fae36ade7d14ccd13e67da58b655391c
Nick Sayer <nsayer@kfu.com>
2021-09-15 14:03:03 -0700

    Make the status request actually give back some meaningful status.

--- sms.py
+++ sms.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
-import serial
-import csv
 import RPi.GPIO as GPIO
-import time
+import csv
+import json
+import random
+import serial
+import subprocess
 import syslog
+import time
+
+random.seed()
 
 ser = serial.Serial('/dev/ttyUSB2')
 # Use this for debugging
@@ -61,6 +66,39 @@ def pollSMS():
 		out.append({'id':int(infos[0]), 'status':infos[1], 'sender':infos[2], 'time':infos[4], 'text':textLine})
 	return out
 
+def doStatus(sender):
+	jsonTxt = subprocess.run(["ip", "-f", "inet", "-j", "address"], capture_output=True, text=True).stdout
+	jsonDat = json.loads(jsonTxt)
+	sTxt = ""
+	for block in jsonDat:
+		if (block['ifname'] == 'lo'):
+			continue
+		iface = block['ifname']
+		for addr_info in block['addr_info']:
+			if (addr_info['family'] != 'inet'):
+				continue
+			address = addr_info['local']
+		sTxt += "Interface " + iface + " has address " + address + "\r"
+
+	res = subprocess.run(["ping", "-c", "3", "-W", "5", "192.168.20.1"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode
+	if (res == 0):
+		sTxt += "router is up." + "\r"
+	else:
+		sTxt += "router is down!" + "\r"
+
+	hosts = ["1.1.1.1", "1.0.0.1", "8.8.8.8", "8.8.4.4", "8.26.56.26", "8.20.247.20", "9.9.9.9", "149.112.112.112", "64.6.64.6", "64.6.65.6"]
+	random.shuffle(hosts)
+	good = False
+	for host in hosts:
+		res = subprocess.run(["ping", "-c", "3", "-W", "5", host], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode
+		if (res == 0):
+			sTxt += host + " is up." + "\r"
+			good = True
+			break
+	if (good == False):
+		sTxt += "All hosts down!" + "\r"
+	sendSMS(sender, sTxt)
+
 def doReboot():
 	syslog.syslog(syslog.LOG_CRIT, "remotely commanded reset of router")
 	GPIO.setmode(GPIO.BCM)
@@ -78,7 +116,7 @@ while True:
 			continue
 		msg = msg[1:]
 		if (msg == 'STATUS'):
-			sendSMS(text['sender'], "Ready.")
+			doStatus(text['sender'])
 		elif (msg == 'REBOOT'):
 			doReboot()
 			sendSMS(text['sender'], "Reboot performed.")
